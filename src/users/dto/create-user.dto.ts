84626129683ebddcf089d1a61765d4504b9fc904
mohamadreza mosalli <mohamadresaaa@gmail.com>
2024-06-24 18:01:36 +0330

    update createUserDto and install classValidator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, Length, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  readonly email: string;
+
+  @Length(3, 30)
+  @Matches(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,}$/, {
+    message: 'invalid username'
+  })
+  readonly username: string;
+
+  @Length(8, 20)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,20}$/, {
+    message: 'invalid password'
+  })
+  readonly password: string;
+}
