f087ee919404e1b721f769a0bfe16bcbdcc59f1d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2024-06-24 12:31:50 +0330

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { EnvModule } from './env/env.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CommonModule, DatabaseModule, EnvModule, AuthModule],
+  imports: [CommonModule, DatabaseModule, EnvModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
