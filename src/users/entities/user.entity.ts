84e0210962f25bba640229c0f97eeb9c3ae30e4e
mohamadreza mosalli <mohamadresaaa@gmail.com>
2024-06-24 12:35:42 +0330

    install typeorm and add fields to user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 50, nullable: true, select: false })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true, length: 30 })
+  username: string;
+
+  @Column({ select: false })
+  password: string;
+}
